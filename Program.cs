b072a28c2247a74f916e35eafc51998c3283d2a0
Rakesh Gangwar <rakesh.gangwar@ardanuy.com>
2024-11-22 11:38:49 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using APIConsole.Helpers;
+using System;
+using System.IO;
+
+namespace APIConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var basePath = CommonHelper.BasePath() + @"\App_Data\HB\";
+            var objBL = new TravayooBO();
+            var data = objBL.GetSearchLogAsync("Test", 21);
+            int index = 0;
+            foreach (var item in data.Result)
+            {
+
+                if (!string.IsNullOrEmpty(item.Request))
+                {
+                    File.WriteAllText(basePath + string.Format("HBSRESP-{0}.json", DateTime.Now.Ticks), item.Request);
+                }
+
+                ++index;
+            }
+
+        }
+    }
+}
---
+++ TravayooBO.cs
@@ -0,0 +1,92 @@
+﻿using APIConsole.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIConsole
+{
+
+    public class APILogModel
+    {
+        public int Supplier { get; set; }
+        public string TrackNo { get; set; }
+        public string Request { get; set; }
+        public string Response { get; set; }
+        public string CallType { get; set; }
+        public int LogId { get; set; }
+    }
+
+
+
+
+
+
+
+    public class TravayooBO
+    {
+
+        string constr = ConfigurationManager.ConnectionStrings["INGMContext"].ConnectionString;
+        public async Task<List<APILogModel>> GetSearchLogAsync(string trackNo, int supplier)
+        {
+           var lstLog = new List<APILogModel>();
+            string sql = @"select * from tblSimulationResult where resultid in (1,2)";
+            using (var conn = new SqlConnection(constr))
+            using (var cmd = conn.CreateCommand())
+            {
+                conn.Open();
+                cmd.CommandText = sql;
+                //cmd.Parameters.AddWithValue("@id", supplier);
+                using (var reader = await cmd.ExecuteReaderAsync())
+                {
+                    if (reader.Read())
+                    {
+                        var obj = new APILogModel();
+                        obj.Supplier = supplier;
+                        obj.TrackNo = trackNo;
+                        obj.Request = reader.GetString(reader.GetOrdinal("ResultXML"));
+                        lstLog.Add(obj);
+                    }
+                }
+            }
+            return lstLog.OrderBy(x=>x.LogId).ToList();
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
